29908cd0d57afc07c670bc7fffa493a3e359a4eb
addisu <>
2025-04-09 21:25:00 +0300

    first

---
+++ csjsimscore/src/domain/FieldOfStudy.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author cslab
+ */
+public class FieldOfStudy {
+    int id;
+    int departementid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDepartementid() {
+        return departementid;
+    }
+
+    public void setDepartementid(int departementid) {
+        this.departementid = departementid;
+    }
+    
+}
